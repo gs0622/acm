4cb826cbb3c10905ad7c207d6c22a3ef435c4fe9
Harry Pan <harry.pan@intel.com>
2015-10-28 18:37:27 +0800

    naive FFDH

--- 2000.c
+++ 2000.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#define MAX(a, b)	((a>b)?a:b)
 #define MIN(a, b)	((a>b)?b:a)
+int ffdh(int h, int w) {
+	static int H = 0, W = 0;
+	int min = MIN(w, h), max = MAX(w, h);
+	if (H + min < 1800) H += min, W = max;
+	else H = min, W += max;
+	return W;
+}
 int main(void) {
-	int i, n;
+	int i, n, min;
 	scanf("%d", &n);
-	int height[n], width[n], thick[n], cover[n];
+	int height[n], width[n], thick[n], H[n], W[n];
 	for (i=0; i<n; i++) {
 		scanf("%d %d %d", &height[i], &width[i], &thick[i]);
-		cover[i] = MIN((width[i]*2+thick[i]),(height[i])*2) + 80;
+		H[i] = width[i]*2 + thick[i] + 80;
+		W[i] = height[i];
 	}
-	for (i=0; i<n; i++) printf("%d\n", cover[i]);
-	int min = cover[0];
-	for (i=1; i<n; i++) if (min > cover[i]) min = cover[i];
+	//for (i=0; i<n; i++) printf("%d %d\n", H[i], W[i]);
+	for (i=0; i<n; i++) min = ffdh(H[i], W[i]);
 	printf("%d\n", min);
 	// TBD: retangle packing algorithm
 }
